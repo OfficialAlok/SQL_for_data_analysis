7bc0c925e0fef0992e2a6c4dfb7391a4db3800b0
OfficialAlok <sahalok001@gmail.com>
2024-12-01 11:27:42 +0530

    Closes #3 removed sales from 2013 and 2017 because they consist data of only single month. 2013 has data of only december and 2017 has data of only january

--- date.sql
+++ date.sql
@@ -1,12 +1,13 @@
 -- Question 1
-SELECT DATE_TRUNC('year', occurred_at), SUM(total_amt_usd) total_sales
+SELECT DATE_PART('year', occurred_at) ord_year, SUM(total_amt_usd) total_sales
 FROM orders
 GROUP BY 1
 ORDER BY 2 DESC;
 
 -- Question 2
-SELECT DATE_PART('month', occurred_at), SUM(total_amt_usd) total_sales
+SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_sales
 FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
 GROUP BY 1
 ORDER BY 2 DESC
 
