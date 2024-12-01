08b7d42c59698cb26ded64c87e4b1d1e184dbeae
OfficialAlok <sahalok001@gmail.com>
2024-12-01 11:39:01 +0530

    DATE_TRUNC and DATE_PART explored

--- date.sql
+++ date.sql
@@ -25,10 +25,11 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 -- Question 5
-SELECT DATE_TRUNC('month', occurred_at), SUM(gloss_amt_usd) total_gloss_amt
+SELECT DATE_TRUNC('month', occurred_at) ord_date, SUM(gloss_amt_usd) total_gloss_amt
 FROM orders o
 JOIN accounts a
 ON o.account_id = a.id
 WHERE a.name = 'Walmart'
 GROUP BY 1
-ORDER BY 2 DESC;
+ORDER BY 2 DESC
+LIMIT 1;
