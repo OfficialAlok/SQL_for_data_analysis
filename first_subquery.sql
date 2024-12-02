be6aea222d99ffb8c93fc99bb0c64f8ada7902dd
OfficialAlok <sahalok001@gmail.com>
2024-12-02 20:21:30 +0530

    pulling data from subequeried table

--- first_subquery.sql
+++ first_subquery.sql
@@ -5,4 +5,17 @@ FROM
        COUNT(*)
 FROM web_events
 GROUP BY 1, 2
+ORDER BY 3 DESC) sub;
+
+
+-- Explored further
+SELECT channel,
+       AVG(count)
+FROM 
+(SELECT DATE_TRUNC('day', occurred_at),
+       channel,
+       COUNT(*)
+FROM web_events
+GROUP BY 1, 2
 ORDER BY 3 DESC) sub
+GROUP BY 1;
