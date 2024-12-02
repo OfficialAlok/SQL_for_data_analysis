eb0f8ed1df80714b2358031120eba028e3cbca42
OfficialAlok <sahalok001@gmail.com>
2024-12-02 20:26:49 +0530

    revamped structure of subqueries

--- first_subquery.sql
+++ first_subquery.sql
@@ -1,8 +1,8 @@
 SELECT *
 FROM 
-(SELECT DATE_TRUNC('day', occurred_at),
+(SELECT DATE_TRUNC('day', occurred_at) AS day,
        channel,
-       COUNT(*)
+       COUNT(*) AS events
 FROM web_events
 GROUP BY 1, 2
 ORDER BY 3 DESC) sub;
@@ -10,12 +10,11 @@ ORDER BY 3 DESC) sub;
 
 -- Explored further
 SELECT channel,
-       AVG(count)
+       AVG(events) AS average_events
 FROM 
-(SELECT DATE_TRUNC('day', occurred_at),
-       channel,
-       COUNT(*)
+(SELECT DATE_TRUNC('day', occurred_at) AS day,
+       channel, COUNT(*) events
 FROM web_events
-GROUP BY 1, 2
-ORDER BY 3 DESC) sub
-GROUP BY 1;
+GROUP BY 1, 2) sub
+GROUP BY 1
+ORDER BY 2 DESC;
