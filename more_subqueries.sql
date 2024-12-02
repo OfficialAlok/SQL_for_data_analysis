1a88985902f316aa08aed316e766f60b25a1a325
OfficialAlok <sahalok001@gmail.com>
2024-12-02 20:54:06 +0530

    instead of taking all rows and columns used the subqueries with WHERE clause

--- more_subqueries.sql
+++ more_subqueries.sql
@@ -1,8 +1,5 @@
-SELECT *
-FROM orders
-WHERE DATE_TRUNC('month', occurred_at) = (
-    SELECT DATE_TRUNC('month', min(occurred_at))
-    FROM orders);
+SELECT DATE_TRUNC('month', min(occurred_at))
+FROM orders;
 
 
 -- Query 2
@@ -10,10 +7,7 @@ SELECT AVG(standard_qty) average_standard,
        AVG(gloss_qty) average_gloss,
        AVG(poster_qty) average_poster,
        SUM(total_amt_usd) total_amount
-FROM (
-    SELECT *
-    FROM orders
-    WHERE DATE_TRUNC('month', occurred_at) = (
-        SELECT DATE_TRUNC('month', min(occurred_at))
-        FROM orders)
-        ) sub; 
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+    (SELECT DATE_TRUNC('month', min(occurred_at))
+    FROM orders);
