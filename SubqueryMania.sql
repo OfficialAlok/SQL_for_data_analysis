0258d56a36894abf1d1d9998fc324e49ab63db16
OfficialAlok <sahalok001@gmail.com>
2024-12-03 18:59:33 +0530

    corrections on subqueries

--- SubqueryMania.sql
+++ SubqueryMania.sql
@@ -28,7 +28,9 @@ FROM
 ON t2.total = t1.total;
 
 -- Question 2
-SELECT r.name region, SUM(o.total_amt_usd) total, COUNT(o.*)
+SELECT region, total_count
+FROM (
+SELECT r.name region, SUM(o.total_amt_usd) total, COUNT(o.*) total_count
 FROM sales_reps s
 JOIN region r
 ON r.id = s.region_id
@@ -37,27 +39,29 @@ ON a.sales_rep_id = s.id
 JOIN orders o
 ON o.account_id = a.id
 GROUP BY 1
-ORDER BY 2 DESC
+ORDER BY 2 DESC) t1
 LIMIT 1;
 
 -- Question 3
-SELECT id, name, total
+SELECT COUNT(*)
 FROM (
-    SELECT a.id, a.name, SUM(total) total
-    FROM accounts a
-    JOIN orders o
-    ON o.account_id = a.id
-    GROUP BY 1, 2
-    ) t1
-WHERE total > (
-    SELECT SUM(o.standard_qty) total_std
-    FROM accounts a
-    JOIN orders o
-    ON o.account_id = a.id
-    GROUP BY a.name
-    ORDER BY 1 DESC
-    LIMIT 1
-    ); 
+SELECT a.id, a.name
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY 1, 2
+HAVING SUM(total)> (
+    SELECT total
+    FROM (
+        SELECT SUM(o.standard_qty) total_std, SUM(o.total) total
+        FROM accounts a
+        JOIN orders o
+        ON o.account_id = a.id
+        GROUP BY a.name
+        ORDER BY 1 DESC
+        LIMIT 1)
+    )
+);
 
 -- Question 4
 SELECT a.name, w.channel, count(*)
@@ -88,13 +92,13 @@ FROM (
     LIMIT 10) t1;
 
 -- Qustion 6
-SELECT AVG(total) lifetime_average
+SELECT AVG(avg_total) lifetime_average
 FROM (
-    SELECT a.id, a.name, AVG(total_amt_usd) total
+    SELECT a.id, a.name, AVG(total_amt_usd) avg_total
     FROM accounts a
     JOIN orders o
     ON o.account_id = a.id
-WHERE total > (
+    GROUP BY 1, 2
+    HAVING AVG(total_amt_usd) > (
     SELECT AVG(total_amt_usd) avg_total
-    FROM orders)
-    GROUP BY 1, 2) temp;
+    FROM orders)) temp;
