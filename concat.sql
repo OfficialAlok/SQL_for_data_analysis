f6df656776850a5c0bab68f04a52cf7bf4ecff4b
OfficialAlok <sahalok001@gmail.com>
2024-12-05 11:21:17 +0530

    concat revamped

--- concat.sql
+++ concat.sql
@@ -1,16 +1,18 @@
 -- Question 1
-SELECT primary_poc,
-       LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) ||
-       '.' ||
-       RIGHT(primary_poc, LENGTH(primary_poc) -  STRPOS(primary_poc, ' ')) || '@' || name || '.com' AS email
-FROM accounts;
+With t1 AS (SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) first_name,
+                RIGHT(primary_poc, LENGTH(primary_poc) -  STRPOS(primary_poc, ' ')) last_name,
+                name
+            FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name,'@',name)
+FROM t1;
 
 -- Question 2
-SELECT primary_poc,
-       LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) ||
-       '.' ||
-       RIGHT(primary_poc, LENGTH(primary_poc) -  STRPOS(primary_poc, ' ')) || '@' || REPLACE(name,' ','') || '.com' AS email
-FROM accounts;
+With t1 AS (SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) first_name,
+                RIGHT(primary_poc, LENGTH(primary_poc) -  STRPOS(primary_poc, ' ')) last_name,
+                name
+            FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name,'@',REPLACE(name,' ',''))
+FROM t1;
 
 -- Question 3
 SELECT primary_poc,
